71db3e08cc33dac6233065c7719009f3265bfb9f
jeferson-felipe-bezerra <133612329+JEFERSON-FELIPE@users.noreply.github.com>
2023-05-23 03:49:03 -0300

    atualização

--- storage/framework/views/29cbd01fbbb18b90395fe4bc24e40f329c4dde64.php
+++ storage/framework/views/29cbd01fbbb18b90395fe4bc24e40f329c4dde64.php
@@ -7,6 +7,9 @@
     <title>Evento</title>
     <link rel="stylesheet" href="\style.css">
     <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css" />
+    <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
+
 </head>
 <body>
     <article  class="inline text-right  ">
@@ -25,72 +28,175 @@
         </div>
     </article>
 
+            <!--tema -->
+            <section class="w-[80%] ml-[10%]">
+                <div class="mx-auto text-center">
+                  <img src="/img/novo texto.png" alt="Imagem" class="w-full h-auto">
+                </div>
+              </section>
+
+
             <!---->
-
-          <div id="imagem2" class="w-[100%] h-[350pxphp]" >
-
-          </div>
-
-            <!---->
-            <section>
-                <div id="caixa-texto"  class=" border border-white-500 border-2 rounded-[20px]  w-[70%] h-[250px] ml-[15%] mt-[10%] " >
-                <p id="texto_em_breve" class="text-xl text-white mt-[10%] ">
-                   EM BREVE NOVIDADES
-
-
-                </p>
-
+            <!--video +texteo-->
+          <section class="flex flex-col items-center text-center w-full">
+            <div class="w-[85%] bg-gray-100 rounded-lg p-8">
+              <div class="flex flex-col md:flex-row">
+                <div class="w-full md:w-1/2">
+                  <div class="relative" style="padding-bottom: 85%;">
+                    <video class="absolute top-0 left-0 w-full h-full rounded-lg shadow-lg" style="width: 100%; height: 100%;" src="/img/WhatsApp Video 2023-05-22 at 13.33.27.mp4" controls></video>
+                  </div>
+                </div>
+                <div class="w-full md:w-1/2 px-1">
+                  <p class="text-gray-800 text-lg font-semibold">O Resgate do Verdadeiro Poder Feminino ✨</p>
+                  <p class="text-gray-600 mt-4">No próximo dia 16 de Julho, mais de mil mulheres estarão juntas no Teatro Riachuelo para o 7º Encontro Mulheres que Marcam, o maior e mais impactante evento de empreendedorismo feminino do Nordeste.</p>
+                  <p class="text-gray-800 text-lg font-semibold">Teremos 8h de treinamento, das 10 às 19h, e uma trilha fantástica que irá mesclar conteúdo teórico-prático, com momentos de inspiração e muita conexão feminina.</p>
+                  <p class="text-gray-600 mt-4"><b>O propósito desse grande momento será desenvolver de forma sistêmica a Identidade Feminina, fortalecendo a compreensão que cada mulher tem sobre si.</b></p>
+                  <p class="text-gray-800 text-lg font-semibold">Através de uma trilha integrada e uma abordagem interdisciplinar, as treinadoras oficiais do 7º Encontro Mulheres que Marcam irão desenvolver temáticas, aplicar ferramentas especiais e promover dinâmicas interativas.</p>
+                  <p class="text-gray-800 text-lg font-semibold">O objetivo é aprofundar a percepção que a mulher tem sobre si e a forma como se posiciona em seu mercado.</p>
+                  <p class="text-gray-800 text-lg font-semibold">O Resgate do Verdadeiro Poder Feminino ✨</p>
+                </div>
+              </div>
             </div>
-        </div>
+          </section>
 
-            </section>
+          <!--carrossel-->
+
+            <section class="w-[100%] inline-block">
+                <div id="inicio" class="w-[100%] my-[40px] text-center text-2xl"></div>
+                <div class="swiper-container">
+                  <div class="swiper-wrapper">
+                    <div id="imagem_1" class="swiper-slide w-[35%] h-[355px] inline-block border border-solid border-black rounded-lg mx-10 my-10 transform hover:scale-110 transition-transform duration-300">
+
+                    </div>
+                    <div id="imagem_2" class="swiper-slide w-[35%] h-[355px] inline-block border border-solid border-black rounded-lg mx-10 my-10 transform hover:scale-110 transition-transform duration-300">
+
+                    </div>
+                    <div id="imagem_3" class="swiper-slide w-[35%] h-[355px] inline-block border border-solid border-black rounded-lg mx-10 my-10 transform hover:scale-110 transition-transform duration-300">
+
+                    </div>
+                    <div id="imagem_4" class="swiper-slide w-[35%] h-[355px] inline-block border border-solid border-black rounded-lg mx-10 my-10 transform hover:scale-110 transition-transform duration-300">
+
+                    </div>
+                    <div id="imagem_5" class="swiper-slide w-[35%] h-[355px] inline-block border border-solid border-black rounded-lg mx-10 my-10 transform hover:scale-110 transition-transform duration-300">
+
+                    </div>
+                    <div id="imagem_6" class="swiper-slide w-[35%] h-[355px] inline-block border border-solid border-black rounded-lg mx-10 my-10 transform hover:scale-110 transition-transform duration-300">
+
+                    </div>
+                    <div id="imagem_7" class="swiper-slide w-[35%] h-[355px] inline-block border border-solid border-black rounded-lg mx-10 my-10 transform hover:scale-110 transition-transform duration-300">
+
+                    </div>
+                    <div id="imagem_8" class="swiper-slide w-[35%] h-[355px] inline-block border border-solid border-black rounded-lg mx-10 my-10 transform hover:scale-110 transition-transform duration-300">
+
+                    </div>
+                    <div id="imagem_9" class="swiper-slide w-[35%] h-[355px] inline-block border border-solid border-black rounded-lg mx-10 my-10 transform hover:scale-110 transition-transform duration-300">
+
+                    </div>
+                  </div>
+                  <div class="swiper-pagination"></div>
+                </div>
+              </section>
+
+               <!---->
+
+                <!--programação-->
+            <section class="flex text-left w-full">
+                <div class="w-[100%] bg-gray-100 rounded-lg p-8">
+                  <div class="flex items-center">
+                    </div>
+                    <div class="w-[100%] px-1">
+                        <p class="text-center text-4xl p-[20px]"><b>Confira a Programação</b></p>
+
+                      <ul>
+                          <li class="text-black-600 mt-4 text-xl">⭐️ <b>Kênia Raissa</b> <br>
+                            Estrategista em Marcas Pessoais Femininas, Treinadora de Treinadores Globais e Empresária. <br>
+                            PONTO DE CONVERGÊNCIA: O PODEROSO ENCONTRO DA SUA HISTÓRIA DE VIDA COM SEU PROPÓSITO </li>
+                            <br>
+                          <li class="text-black-600 mt-4 text-xl">⭐️ <b>Danielle Mafra</b> <br>
+                            Executiva em ESG e atual Secretária Executiva do Município de Natal.
+
+                            FUTURO, TENDÊNCIAS E O NOSSO PAPEL NA NOVA ECONOMIA</li>
+                            <br>
+                          <li class="text-black-600 mt-4 text-xl"> ⭐️ <b>Katarina Alcântara</b> <br>
+                            Personal Business, Mentora de Carreiras e Treinadora Comportamental. <br>
+
+                            COMPETÊNCIAS INTERPESSOAIS QUE COLOCAM A MULHER CONTEMPORÂNEA EM POSIÇÃO DE DESTAQUE </li>
+                            <br>
+                          <li class="text-black-600 mt-4 text-xl"> ⭐️ <b>Laíse Trindade</b>
+                            <br>
+                            Nutricionista, Especialista em Neurociência aplicada ao Desenvolvimento Humano. <br>
+
+                            PRODUTIVIDADE ESTRATÉGICA E ALTA PERFORMANCE NA VIDA E NOS NEGÓCIOS</li>
+                            <br>
+                          <li class="text-black-600 mt-4 text-xl">
+                            ⭐️ <b>Priscila Barros</b> <br>
+                            Estrategista Digital e Mentora em Posicionamento Estratégico Digital. </li>
+                            <br>
+                          <li class="text-black-600 mt-4 text-xl">⭐️ <b>Thaís Barbosa</b> <br>
+                            Mentora e Treinadora de Experts, Especialista em Método, Narrativas e Criação de Produtos e Serviços Educacionais. <br>
+
+                            COMO SUPERAR O MEDO DA EXPOSIÇÃO DIGITAL E TER A CORAGEM DE CRIAR UM POSICIONAMENTO AUTÊNTICO  </li>
+                            <br>
+                          <li class="text-black-600 mt-4 text-xl">⭐️ <b>Aline Chacon</b> <br>
+                            Educadora e Mentora de Mulheres que buscam viver sua autenticidade. <br>
+
+                            MULHER REAL E CONTEMPORÂNEA: LIBERTE-SE DE QUEM VOCÊ NÃO É PARA VIVER SUA VERDADEIRA IDENTIDADE </li>
+                            <br>
+                          <li class="text-black-600 mt-4 text-xl">⭐️ <b>Ísis Moreira</b> <br>
+                            Treinadora de Negócios e Espiritualidade, com mais de 20 mil alunos treinados em Marketing Digital e Desenvolvimento Pessoal. <br>
+
+                            DESENVOLVA INTENCIONALMENTE UMA EXISTÊNCIA DE ABUNDÂNCIA</li>
+                            <br>
+
+                            <p class="text-black-600 mt-4 text-xl">Venha fazer parte desse momento que irá marcar a história de muitas mulheres.</p>
+                            <p class="text-black-600 mt-4 text-xl"><b>Vamos todas juntas!</b></p>
+
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+
+              </section>
             <!---->
-        <section class="w-[100%] block ">
-        <div id="inicio" class="w-[100%]   mt-[10%]  text-center  text-2xl "></div>
-        <article class="w-[85%] mx-[6%]  inline-block flex justify-center">
-            <div id="imagem_1"  class="w-[520px] h-[350px]  inline-block border border-solid border-black rounded-lg mx-7 transform hover:scale-110 transition-transform duration-300"> <p></p></div>
-            <div id="imagem_2" class="w-[520px] h-[350px] inline-block border border-solid border-black rounded-lg mx-7 transform hover:scale-110 transition-transform duration-300"><p></p></div>
-            <div id="imagem_3"  class="w-[520px] h-[350px]  inline-block border border-solid border-black rounded-lg mx-7 transform hover:scale-110 transition-transform duration-300"><p></p></div>
-        </article>
 
-        <section class="w-[75%] mx-[13%] mt-[15%] inline-block flex justify-center ">
+                <!--botões-->
+            <section class="w-[75%] mx-[10%] mt-[15%] inline-block flex justify-center flex-col md:flex-row">
 
-
-            <div class="w-[45%] h-[30%] inline-block  float-left m-[20px]" >
-                <div id="caixa-texto"  class=" border border-white-500 border-2 rounded-[20px]  w-[450px] h-[250px] " >
-                        <div class="ml-[40%] p-[15px]" > <img src="/img/whatsapp (7).png" alt=""></div>
+                <div class="w-[100%] md:w-[40%] h-[30%] inline-block md:float-left md:m-[40px] mb-6 md:mb-0">
+                  <div id="caixa-texto" class="border border-white-500 border-2 rounded-[20px] w-[450px] h-[250px]">
+                    <div class="ml-[40%] p-[15px]">
+                      <img src="/img/whatsapp (7).png" alt="">
+                    </div>
                     <p class="text-xl text-white">
-                        Participar do Grupo Aberto no WhatsApp
-                        <br>
-                        <a href="https://chat.whatsapp.com/BxwcQ1emcq14J0GwZ927IO" target="_blanck"><button id="botao-participar"> QUERO PARTICIPAR</button></a>
-
+                      Participar do Grupo Aberto no WhatsApp
+                      <br>
+                      <a href="https://chat.whatsapp.com/BxwcQ1emcq14J0GwZ927IO" target="_blank">
+                        <button id="botao-participar">QUERO PARTICIPAR</button>
+                      </a>
                     </p>
-
-                </div>
-            </div>
-
-
-            <div class="w-[45%] h-[30%] inline-block  m-[20px]" >
-                <div id="caixa-texto" class=" border border-white-500 border-2 rounded-[20px]  p-[20px] w-[450px] h-[250px]" >
-                    <p class="text-xl">Cadastre seu EMAIL para receber os
-                     UPDATES em primeira-mão!</p>
-                    <form action="" method="POST">
-                    <label for="nome">Nome:</label>
-                    <input type="text" id="nome" name="nome" required><br><br>
-
-                    <label for="email">E-mail:</label>
-                    <input type="email" id="email" name="email" required><br><br>
-
-                    <input type="checkbox" id="aceitar" name="aceitar" required>
-                    <label for="aceitar">Eu li e concordo com os termos de privacidade</label><br><br>
-
-                    <input type="submit" value="Enviar">
-                  </form>
+                  </div>
                 </div>
 
-
+                <div class="w-[100%] md:w-[40%] h-[30%] inline-block md:float-left md:m-[40px]">
+                  <div id="caixa-texto" id="inscrever" class="border border-white-500 border-2 rounded-[20px] w-[450px] h-[250px]">
+                    <div class="ml-[40%] p-[35px]">
+                      <img src="" alt="">
+                    </div>
+                    <p class="text-4xl text-white">
+                      FAÇA JÁ SUA INSCRIÇÃO !
+                      <br>
+                      <a href="https://sun.eduzz.com/1827710" target="_blank">
+                        <button id="botao-participar" class="text-4xl">CLICK AQUI!</button>
+                      </a>
+                    </p>
+                  </div>
                 </div>
-            </div>
+
+              </section>
+
+
+
+
 
         </section>
 
@@ -102,6 +208,92 @@
             <p >Todos os direitos reservados </p>
         </div>
     </footer>
+    <!-- Adicione o CSS e o HTML fornecidos anteriormente aqui -->
+
+<!-- Adicione o CSS e o HTML fornecidos anteriormente aqui -->
+
+<!-- Adicione o CSS e o HTML fornecidos anteriormente aqui -->
+
+<style>
+    .swiper-container {
+      overflow: hidden; /* Esconde o conteúdo que transborda */
+    }
+
+    .swiper-wrapper {
+      display: flex; /* Altera o comportamento dos slides para serem exibidos em uma linha */
+      flex-wrap: nowrap; /* Impede que os slides quebrem para a próxima linha */
+    }
+
+    .swiper-slide {
+      flex: 0 0 auto; /* Impede que os slides aumentem além do tamanho do conteúdo */
+    }
+  </style>
+
+ <!-- Adicione o CSS e o HTML fornecidos anteriormente aqui -->
+
+<style>
+    .swiper-container {
+      overflow: hidden; /* Esconde o conteúdo que transborda */
+    }
+
+    .swiper-wrapper {
+      display: flex; /* Altera o comportamento dos slides para serem exibidos em uma linha */
+      flex-wrap: nowrap; /* Impede que os slides quebrem para a próxima linha */
+    }
+
+    .swiper-slide {
+      flex: 0 0 auto; /* Impede que os slides aumentem além do tamanho do conteúdo */
+    }
+  </style>
+
+  <!-- Adicione o CSS e o HTML fornecidos anteriormente aqui -->
+
+<style>
+    .swiper-container {
+      overflow: hidden; /* Esconde o conteúdo que transborda */
+    }
+
+    .swiper-wrapper {
+      display: flex; /* Altera o comportamento dos slides para serem exibidos em uma linha */
+      flex-wrap: nowrap; /* Impede que os slides quebrem para a próxima linha */
+    }
+
+    .swiper-slide {
+      flex: 0 0 auto; /* Impede que os slides aumentem além do tamanho do conteúdo */
+    }
+  </style>
+
+  <script>
+    // Aguarde o carregamento completo do DOM antes de iniciar o Swiper
+    document.addEventListener("DOMContentLoaded", function () {
+      // Inicialize o Swiper
+      var swiper = new Swiper(".swiper-container", {
+        slidesPerView: "auto", // Define o número de slides visíveis
+        spaceBetween: 10, // Define o espaçamento entre os slides
+        loop: true, // Habilita o loop do carrossel
+        navigation: {
+          nextEl: ".swiper-button-next", // Elemento HTML para o botão "próximo"
+          prevEl: ".swiper-button-prev" // Elemento HTML para o botão "anterior"
+        },
+        autoplay: {
+          delay: 2000, // Tempo de espera entre cada slide (em milissegundos)
+          disableOnInteraction: false // Continua o autoplay mesmo quando o usuário interage com o carrossel
+        },
+        speed: 500 // Ajusta a velocidade do carrossel (em milissegundos)
+      });
+
+      // Ajusta a largura dos slides para corresponder ao layout inicial
+      swiper.on("resize", function () {
+        swiper.slides.css("width", ""); // Redefine a largura dos slides para o valor padrão
+        swiper.update(); // Atualiza o Swiper para aplicar as alterações
+      });
+    });
+  </script>
+
+
+
+
+
 
 </body>
 </html>
